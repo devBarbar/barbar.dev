2d271a40f58cdac6e8d94a5b9e448f4d4ccece5b
Barbar Ahmad <barbarahmad@hotmail.de>
2023-10-01 10:07:17 +0200

    make everythign a draft

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://barbar.dev/',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/devBarbar/barbar.dev',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
